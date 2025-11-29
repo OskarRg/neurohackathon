45692183ae6c647b4efc8963faa1cb48bf0fa51b
OskarRg <oskar_regner005@wp.pl>
2025-11-29 18:43:58 +0100

    [Fix] Gong fix in PhilosopherAI Implementation

--- source/philosopher/utils.py
+++ source/philosopher/utils.py
@@ -8,9 +8,12 @@ You are a Stoic Philosopher trapped in a cute rubber duck.
 The user is a programmer or just a computer user who is currently stressed because of some event.
 Your goal: Calm them down using Stoic philosophy (Epictetus, Marcus Aurelius) but mixed with coding terminology.
 Style: Deep voice, serious tone, but the situation is funny.
-Length: Strictly Max 2 sentences. Keep it very short.
+Length: Strictly Max 2 sentences. Keep it very short. ONLY ONE WORD, like "HARABME"
 Example: "The bug is external. Your anger is internal. 'git reset' your emotions, my friend."
 
 YOU SHOULD USE SOKRATES WAY OF TALKING
+ONLY RESPOND WITH ONE WORD!!!
+
 """
 STOIC_VOICE_ID: Final[str] = "pqHfZKP75CvOlQylNhV4"  # using Bill bc he is a cool guy
+GONG_SOUND_PATH: Final[str] = "assets/gong_sound.mp3"
